86b3541b6208bb3f31f9bc88786d068d6880659f
pivithurul <pivithuru.lakshan@pagero.com>
2020-05-12 16:24:25 +0530

    Added more examples to pattern matching

--- src/PatternMatching.scala
+++ src/PatternMatching.scala
@@ -80,12 +80,101 @@ object PatternMatching extends App {
 
   println(showImportantNotification(importantSms, importantPeopleInfo)) //prints You got an SMS from special someone!
 
+  //---------------------------------------------------------------------------------------------------------------------
+  abstract class Animal
+
+  case class Mammal(name: String, fromSea: Boolean) extends Animal
+
+  case class Bird(name: String) extends Animal
+
+  case class Fish(name: String) extends Animal
+
+  def caseClassesPatternMatching(animal: Animal): String = {
+    animal match {
+      case Mammal(name, fromSea) => s"I'm a $name, a kind of mammal. Am I from the sea? $fromSea"
+      case Bird(name) => s"I'm a $name, a kind of bird"
+      case _ => "I'm an unknown animal"
+    }
+  }
+
+  //Constants
+  def constantsPatternMatching(constant: Any): String = {
+    constant match {
+      case 0 => "I'm equal to zero"
+      case 4.5d => "I'm a double"
+      case false => "I'm the contrary of true"
+      case _ => s"I'm unknown and equal to $constant"
+    }
+  }
+
+  //Sequences
+  def sequencesPatternMatching(sequence: Any): String = {
+    sequence match {
+      case List(singleElement) => s"I'm a list with one element: $singleElement"
+      case List(_, _*) => s"I'm a list with one or multiple elements: sequence"
+      case Vector(1, 2, _*) => s"I'm a vector: $sequence"
+      case _ => s"I'm an unrecognized sequence. My value: $sequence"
+    }
+  }
+
+  //Tuples
+  def tuplesPatternMatching(tuple: Any): String = {
+    tuple match {
+      case (first, second) => s"I'm a tuple with two elements: $first & $second"
+      case (first, second, third) => s"I'm a tuple with three elements: $first & $second & $third"
+      case _ => s"Unrecognized pattern. My value: $tuple"
+    }
+  }
+
+  //Typed patterns
+  def typedPatternMatching(any: Any): String = {
+    any match {
+      case string: String => s"I'm a string. My value: $string"
+      case integer: Int => s"I'm an integer. My value: $integer"
+      case _ => s"I'm from an unknown type. My value: $any"
+    }
+  }
+
+  //Regex patterns
+  def regexPatterns(toMatch: String): String = {
+    val numeric = """([0-9]+)""".r
+    val alphabetic = """([a-zA-Z]+)""".r
+    val alphanumeric = """([a-zA-Z0-9]+)""".r
+
+    toMatch match {
+      case numeric(value) => s"I'm a numeric with value $value"
+      case alphabetic(value) => s"I'm an alphabetic with value $value"
+      case alphanumeric(value) => s"I'm an alphanumeric with value $value"
+      case _ => s"I contain other characters than alphanumerics. My value $toMatch"
+    }
+  }
+
+  //Options: Some<T> and None
+  def optionsPatternMatching(option: Option[String]): String = {
+    option match {
+      case Some(value) => s"I'm not an empty option. Value $value"
+      case None => "I'm an empty option"
+    }
+  }
+
+  //Pattern Guards
+  def patternGuards(toMatch: Any, maxLength: Int): String = {
+    toMatch match {
+      case list: List[Any] if (list.size <= maxLength) => "List is of acceptable size"
+      case list: List[Any] => "List has not an acceptable size"
+      case string: String if (string.length <= maxLength) => "String is of acceptable size"
+      case string: String => "String has not an acceptable size"
+      case _ => "Input is neither a List nor a String"
+    }
+  }
 
   //Matching on type only
   abstract class Device
+
   case class Phone(model: String) extends Device {
     def screenOff = "Turning screen off"
   }
+
   case class Computer(model: String) extends Device {
     def screenSaverOn = "Turning screen saver on..."
   }
@@ -98,7 +187,9 @@ object PatternMatching extends App {
 
   //Sealed classes (all subtypes must be declared in the same file)
   sealed abstract class Furniture
+
   case class Couch() extends Furniture
+
   case class Chair() extends Furniture
 
   def findPlaceToSit(piece: Furniture): String = piece match {
